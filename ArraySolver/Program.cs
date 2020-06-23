e3e03bc2b8f9465ef00abb20916e2286d1c24550
BonBon <BonBon@DESKTOP-15RAB9Q>
2020-06-23 22:45:37 +0300

    small change

--- ArraySolver/Program.cs
+++ ArraySolver/Program.cs
@@ -52,7 +52,7 @@ namespace ArraySolver.ConsoleApp
                     //Console.Write("Are you sure you want to choose this as your login key? [y/n] ");
                     response = Console.ReadKey(false).Key;   // true is intercept key (dont show), false is show
                     if (response != ConsoleKey.Enter)
-                        Console.WriteLine(Environment.NewLine + "Wrong key.");
+                        Console.WriteLine(Environment.NewLine);
 
                 } while (response != ConsoleKey.Y && response != ConsoleKey.N);
 
